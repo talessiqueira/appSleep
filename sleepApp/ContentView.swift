757f33c227596962d0df339e4fdc16fe04bfa053
tales <talessiqueira@icloud.com>
2025-02-21 14:29:33 -0300

    Initial Commit

---
+++ sleepApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sleepApp
+//
+//  Created by Tales on 21/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sleepApp/sleepAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleepAppApp.swift
+//  sleepApp
+//
+//  Created by Tales on 21/02/25.
+//
+
+import SwiftUI
+
+@main
+struct sleepAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
