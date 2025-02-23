6d7b0ba7282f4091bce1f761da9d3947a58449e0
tales <talessiqueira@icloud.com>
2025-02-23 15:58:06 -0300

    App working well.

---
+++ sleepApp/Result.swift
@@ -0,0 +1,18 @@
+//
+//  Result.swift
+//  sleepApp
+//
+//  Created by Tales on 23/02/25.
+//
+
+import SwiftUI
+
+struct Result: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Result()
+}
